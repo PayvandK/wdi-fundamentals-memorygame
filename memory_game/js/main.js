75300bd94b3a915c658605e8def7ab32652eb027
Payvand Khosravi <payvandkhosravi1@gmail.com>
2019-09-08 18:20:35 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,33 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User Flipped" + " " + cardOne);
-console.log("User Flipped" + " " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+cards.length;
+
+var cardsInPlay = [];
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+
+function checkForMatch() {
+	if (cardsInPlay[1] === cardsInPlay[3]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cards.length === 2) {
+		checkForMatch();
+	}
+}
+flipCard(1);
+flipCard(3);
+
+checkForMatch();
