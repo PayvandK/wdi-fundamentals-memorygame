445277878568bf8f75a28056709af6d95e89778b
Payvand Khosravi <payvandkhosravi1@gmail.com>
2019-09-08 22:09:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,57 +1,57 @@
 var cards = [
 {
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png",
 },
 {
-rank: "queen",
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png",
 },
 {
-rank: "king",
-suit: "hearts",
-cardImage: "images/king-of-hearts.png"
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png",
 },
 {
-rank: "king",
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"	
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png",	
 }
 ];
 
-cards.length;
-
 var cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-		console.log("You found a match!");
-	} else {
-		console.log("Sorry, try again.");
-	}
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement); 
+	};
 }
 
 function checkForMatch() {
-	if (cardsInPlay[1] === cardsInPlay[3]) {
-		console.log("You found a match!");
+	var cardId = this.getAttribute('data-id');
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
-	}
+			alert("Sorry, try again.");
+		};
+	};
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	this.setAttribute('src',cards[cardId]);
+	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-
-	if (cards.length === 2) {
-		checkForMatch();
-	}
+	checkForMatch();
 }
-flipCard(1);
-flipCard(3);
 
-checkForMatch();
+createBoard();
